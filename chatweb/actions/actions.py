46e04692748ec42f8e89171878475cb4e51a7e7f
jagan20181998 <jagan.vijay.104@gmail.com>
2022-01-11 16:00:23 +0530

    python files Added

---
+++ chatweb/actions/actions.py
@@ -0,0 +1,50 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+import datetime as dt 
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import EventType
+
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_ask_last_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict) -> List[EventType]:
+        
+        first_name=tracker.get_slot("name")
+        print(first_name)
+        dispatcher.utter_message(text=f"Hi,{first_name} change what are you want")
+        return []
